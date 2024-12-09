569e7a0f3c2d776442144e021ac695a22f5e9ffe
krozfu <carlosuefagh@gmail.com>
2024-12-09 16:29:24 -0500

    se unen cambios para la firma en registrar horas

--- sis/app/src/main/java/com/example/sis/views/CustomBottomBar.kt
+++ sis/app/src/main/java/com/example/sis/views/CustomBottomBar.kt
@@ -4,14 +4,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -55,17 +52,6 @@ fun CustomBottomAppBar(navController: NavController) {
                     modifier = Modifier.size(40.dp)
                 )
             }*/
-
-
-            IconButton(onClick = { navController.navigate("firma") }) {
-                Image(
-                    painter = painterResource(id = R.drawable.notification),
-                    contentDescription = "Notifications",
-                    modifier = Modifier.size(40.dp)
-                )
-            }
-
-
 
             IconButton(onClick = { navController.navigate("profile/${userIdManager.getUserId()}") }) {
                 Image(
